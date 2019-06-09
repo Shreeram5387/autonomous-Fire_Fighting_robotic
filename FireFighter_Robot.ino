bba6dd1e09006692f2b70d76a6f6b1ab8b1f6013
Shreeram5387 <51193286+Shreeram5387@users.noreply.github.com>
2019-06-09 13:48:34 +0530

    Add files via upload

---
+++ FireFighter_Robot.ino
@@ -0,0 +1,216 @@
+//Viral science www.youtube.com/c/viralscience
+//FireFighter Truck
+#include <AFMotor.h>
+#include <NewPing.h>
+#include <Servo.h> 
+
+#define TRIG_PIN A0 
+#define ECHO_PIN A1 
+#define MAX_DISTANCE 300 
+#define MAX_SPEED 190 // sets speed of DC  motors
+#define MAX_SPEED_OFFSET 10
+
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE); 
+
+AF_DCMotor right_motor(4, MOTOR12_8KHZ);
+AF_DCMotor left_motor(3, MOTOR12_8KHZ);
+AF_DCMotor three_motor(1, MOTOR12_8KHZ);
+
+String readString;
+Servo myservo;   
+
+boolean goesForward=false;
+int distance = 100;
+int speedSet = 0;
+void setup() {
+  Serial.begin(9600);
+  right_motor.setSpeed(255);
+  left_motor.setSpeed(255);
+  three_motor.setSpeed(255);
+myservo.attach(9);  
+  myservo.write(70); 
+  delay(2000);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  
+  }
+
+
+void loop() 
+{
+  while(Serial.available()){
+    delay(50);
+    char c=Serial.read();
+    readString+=c;
+  }
+  if(readString.length()>0){
+    Serial.println(readString);
+    if (readString =="FORWARD"){ //forward
+      right_motor.run (FORWARD);
+      left_motor.run (FORWARD);
+      delay(500);
+    }
+    if (readString =="BACKWARD"){ //backward
+      right_motor.run (BACKWARD);
+      left_motor.run (BACKWARD);
+      delay(500);
+    }
+    if (readString =="LEFT"){ //left
+      right_motor.run (FORWARD);
+      left_motor.run (BACKWARD);
+      delay(500);
+    }
+    if (readString =="RIGHT"){  //right
+      right_motor.run (BACKWARD);
+      left_motor.run (FORWARD);
+      delay(500);
+    }
+    if (readString =="STOP"){  //stop
+      right_motor.run (RELEASE);
+      left_motor.run (RELEASE);
+      delay(500);
+    }
+    if (readString =="RUN WATER"){  //ON Water Pump
+      three_motor.run (FORWARD);
+      delay(500);
+    }
+    if (readString =="STOP WATER"){  //OFF Water Pump
+      three_motor.run (RELEASE);
+      delay(500);
+    }
+    if (readString =="AUTO"){  //OFF Water Pump
+int distanceR = 0;
+ int distanceL =  0;
+ delay(40);
+ 
+ if(distance<=50)
+ {
+  moveStop();
+  delay(100);
+  moveBackward();
+  delay(500);
+  moveStop();
+  delay(200);
+
+  distanceR = lookRight();
+  delay(200);
+  distanceL = lookLeft();
+  delay(200);
+
+  if(distanceR>=distanceL)
+  {
+    turnRight();
+    moveStop();
+  }else
+  {
+    turnLeft();
+    moveStop();
+  }
+ }else
+ {
+  moveForward();
+  myservo.write(70); 
+
+ }
+ distance = readPing();
+      delay(500);
+  
+  readString="";
+  }
+ 
+}
+}
+int lookRight()
+{
+    myservo.write(30); 
+    delay(500);
+    int distance = readPing();
+    delay(100);
+    myservo.write(80); 
+    delay(200);
+
+    return distance;
+    
+}
+
+int lookLeft()
+{
+    myservo.write(80); 
+    delay(500);
+    int distance = readPing();
+    delay(100);
+    myservo.write(130); 
+      delay(200);
+    return distance;
+}
+
+int readPing() { 
+  delay(70);
+  int cm = sonar.ping_cm();
+  if(cm==0)
+  {
+    cm = 250;
+  }
+  return cm;
+}
+
+void moveStop() {
+  left_motor.run(RELEASE); 
+  right_motor.run(RELEASE);
+  } 
+  
+void moveForward() {
+
+ if(!goesForward)
+  {
+    goesForward=true;
+    left_motor.run(FORWARD);      
+    right_motor.run(FORWARD); 
+   for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
+   {
+    left_motor.setSpeed(speedSet);
+    right_motor.setSpeed(speedSet+MAX_SPEED_OFFSET);
+    delay(5);
+ 
+   }
+  }
+}
+
+void moveBackward() {
+    goesForward=false;
+    left_motor.run(BACKWARD);      
+    right_motor.run(BACKWARD);  
+  for (speedSet = 0; speedSet < MAX_SPEED; speedSet +=2) // slowly bring the speed up to avoid loading down the batteries too quickly
+  {
+    left_motor.setSpeed(speedSet);
+    right_motor.setSpeed(speedSet+MAX_SPEED_OFFSET);
+    delay(5);
+  
+  }
+}  
+
+void turnRight() {
+  left_motor.run(FORWARD);
+  right_motor.run(BACKWARD);     
+  delay(1000);
+
+//  motor1.run(FORWARD);      
+//  motor2.run(FORWARD);      
+} 
+ 
+void turnLeft() {
+  left_motor.run(BACKWARD);     
+  right_motor.run(FORWARD);     
+  delay(1000);
+  
+//  motor1.run(FORWARD);     
+//  motor2.run(FORWARD);
+}  
+
+
---
+++ finalcode.ino
@@ -0,0 +1,185 @@
+#include <Servo.h> //include Servo library
+#include<SoftwareSerial.h>
+#include <AFMotor.h>
+#include <NewPing.h>
+#define MAX_DISTANCE 200 
+
+#define TRIG_PIN A0   // HC_SR04 trig pin to Arduino pin 3
+#define ECHO_PIN A1   // HC_SR04 echo pin to Arduino pin 2 (this pin is Arduino's interrupt pin 0)
+
+AF_DCMotor motor1(3); //left
+AF_DCMotor motor2(4);  //right
+
+
+
+int sensorPinforwardd = A3; // select the input pin for the LDR
+int sensorPinleftt = A4; // select the input pin for the LDR
+int sensorPinrightt = A5; // select the input pin for the LDR
+int sensorValueforward = 0;
+int sensorValueleft = 0;
+int sensorValueright= 0;
+int led = 7 ;// Output pin for LED
+int buzzer = 5; // Output pin for Buzzer
+
+
+const int collisionThresh   = 50; //threshold for obstacles (in cm)
+                                  // the sensors to the light source. NOTE: this is ANALOG pin 4
+int leftDistance, rightDistance; //distances on either side
+
+Servo panMotor;   //The micro servo with the distance sensor on it
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE); 
+int distance = 0;
+
+
+void setup()
+{
+ pinMode(led, OUTPUT);
+
+pinMode(buzzer,OUTPUT);
+  panMotor.attach(9);     // attache the micro servo
+  panMotor.write(70);     //center the micro servo
+
+  Serial.begin(9600);
+  Serial.println("This monitor will show us where the robot will go");
+int distance = readPing();  //c/call the ping function to get the distance in front of the robot
+delay(200);
+
+int distance = readPing();  //c/call the ping function to get the distance in front of the robot
+delay(200);
+
+int distance = readPing();  //c/call the ping function to get the distance in front of the robot
+delay(200);
+Serial.print("cm:");
+Serial.println(distance)
+}
+
+void loop(){
+
+ int threshold = 100;
+sensorValueleft = analogRead(sensorPinleftt);  //read the left light sensor
+Serial.print("Left:  ");
+Serial.println(sensorValueleft);
+sensorValueright = analogRead(sensorPinrightt);
+Serial.print("Right:  ");
+Serial.print(sensorValueright);
+sensorValueforward = analogRead(sensorPinforwardd);
+Serial.print("Straight:");
+Serial.println(sensorValueforward);
+delay(200);
+ int distance = readPing();  //c/call the ping function to get the distance in front of the robot
+delay(200);
+Serial.print("cm:");
+Serial.println(distance);
+
+  if (distance > collisionThresh) //if path is clear, the robot can use the light sensors to navigate towards the light
+  {
+  // Check the conditions for moving forward
+    if ( sensorValueleft       > threshold     && 
+       sensorValueright      > threshold     && 
+       sensorValueforward    > threshold        ) 
+       {  
+          Serial.print(" GOO Forward");
+          motor1.run(FORWARD);     
+          motor2.run(FORWARD);
+          delay(300);
+        }
+
+  //This condition indicates that the robot has reached the light source,
+  //so it will stop
+  else if ( sensorValueforward    < threshold/2 && 
+         sensorValueforward   <   threshold/7    ) 
+       {  
+          Serial.println("STOP");
+            motor1.run(RELEASE); 
+            motor2.run(RELEASE); 
+          
+          delay(500);
+        }
+        
+  // If the left light sensor value is smaller than that of the right sensor, 
+  // and the difference between the values is large, then
+  // the light source is towards the left, so robot should turn left. 
+  else if ( sensorValueleft       <   threshold &&
+            sensorValueright       >     threshold    ) 
+            { 
+              Serial.print("LEFT");
+                motor1.run(BACKWARD);     
+                motor2.run(FORWARD);     
+                delay(80);
+             
+            }
+
+  // If the right light sensor value is smaller than that of the left sensor, 
+  // and the difference between the values is large, then
+  // the light source is towards the right, so robot should turn right. 
+  else if ( sensorValueright       <  threshold  &&        
+            sensorValueleft       > threshold ) 
+            { 
+                Serial.print("RIGHT");
+                 motor1.run(FORWARD);     
+                motor2.run(BACKWARD);     
+                delay(80);
+            }
+
+  
+  }
+  else    // The path is blocked, so the robot will use the distance sensor to find a clear path
+  {
+      Serial.print(" blocked ");
+      motor1.run(RELEASE);     
+      motor2.run(RELEASE);       // Stop the right motor
+      panMotor.write(0); 
+      delay(500);
+      rightDistance = readPing();  //scan to the right rightDistance
+      delay(300);
+      panMotor.write(150);
+      delay(500);
+      leftDistance = readPing(); //scan to the left
+      delay(300);
+      panMotor.write(70);     //return to center
+      delay(100);
+     compareDistance();
+  }
+  
+
+}
+
+void compareDistance()
+{
+  if (leftDistance > rightDistance) //if left is less obstructed 
+  {
+     motor1.run(BACKWARD);     
+     motor2.run(FORWARD);
+    Serial.print("Left");
+    delay(400); 
+    motor1.run(FORWARD);     
+     motor2.run(FORWARD);
+    delay(600);
+  }
+  else if (rightDistance > leftDistance) //if right is less obstructed
+  {   
+     motor1.run(FORWARD);     
+     motor2.run(BACKWARD);
+    Serial.print("Right");
+    delay(400);
+     motor1.run(FORWARD);     
+     motor2.run(FORWARD);
+    delay(600);
+  }
+   else //if they are equally obstructed
+  {
+     motor1.run(BACKWARD);     
+     motor2.run(FORWARD);
+    Serial.print("Back");
+    delay(400);
+  }
+}
+
+int readPing() { 
+  delay(70);
+  int cm = sonar.ping_cm();
+  if(cm==0)
+  {
+    cm = 250;
+  }
+}
